1bd6fa7c8a38842be7c1a50002e2317b1bbad18d
Oksana Vasilevskaya <vasilvski1@rambler.ru>
2022-06-21 03:45:32 +0800

    Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Контрольная задача.
+// Напишите программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна три символа
